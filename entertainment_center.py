0be0a54d43108e6030f63427026f05fd932dbef5
Don Chalanga Kuruppu <donchalangakuruppu@Dons-MacBook-Pro.local>
2017-07-16 01:11:03 -0500

    Changed the name of a parameter in the Movie constructor. Created six movie objects in entertainment center.py

---
+++ entertainment_center.py
@@ -0,0 +1,47 @@
+import media
+import fresh_tomatoes
+
+def main():
+
+    # Creating movie object for Inception
+    inception = media.Movie('Inception',
+                            """https://images-na.ssl-images-amazon.com/images/
+                            M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@.
+                            _""V1_UX182_CR0,0,182,268_AL_.jpg""",
+                            'https://www.youtube.com/watch?v=8hP9D6kZseM')
+
+    # Creating movie object for Wonder Woman
+    wonder_woman = media.Movie('Wonder Woman',
+                               """http://t1.gstatic.com/images?q=tbn:ANd9GcQcCA
+                               Omt'-FsRsR8GebIzI67qSvdQ2JLYDRLxeAcbH-541fzqq
+                               1H""",
+                               'https://www.youtube.com/watch?v=VSB4wGIdDwo')
+
+    # Creating movie object for Thor 3
+    thor_3 = media.Movie('Thor 3',
+                         """http://pre13.deviantart.net/4a42/th/pre/f/2016/044
+                         /7/3/thor_3_poster_by_tldesignn-d9rlt8y.png""",
+                         'https://www.youtube.com/watch?v=F7ayGFHGqeQ')
+
+    #Creating movie object for The Dark Knight
+    the_dark_knight = media.Movie('The Dark Knight',
+                                  """""",
+                                  """https://www.youtube.com/watch?v=EXeTwQWr
+                                  cwY""")
+
+    #Creating movie object for Arrival
+    arrival = media.Movie('Arrival',
+                          """https://images-na.ssl-images-amazon.com/images
+                          /M/MV5BMTExMzU0ODcxNDheQTJeQWpwZ15BbWU4MDE1OTI4Mz
+                          Ay._V1_UX182_CR0,0,182,268_AL_.jpg""",
+                          'https://www.youtube.com/watch?v=tFMo3UJ4B4g')
+
+    # Creating movie object for The Martian
+    the_martian = media.Movie('The Martian',
+                              """https://images-na.ssl-images-amazon.com/
+                              images/M/MV5BMTc2MTQ3MDA1Nl5BMl5BanBnXkFtZT
+                              gwODA3OTI4NjE@._V1_UY1200_CR90,0,630,1200_AL
+                              _.jpg""",
+                              'https://www.youtube.com/watch?v=ej3ioOneTy8')
+
+
--- media.py
+++ media.py
@@ -3,11 +3,11 @@ import webbrowser
 class Movie():
 
     # Constructor to populate instance variables of a movie object
-    def __init__(self, movie_title, box_art_url, trailer_youtube_link):
+    def __init__(self, movie_title, image_url, trailer_youtube_link):
         self.title = movie_title
-        self.box_art = box_art_url
-        self.trailer = trailer_youtube_link
+        self.box_art_url = image_url
+        self.trailer_url = trailer_youtube_link
 
-    # Method to show movie trailer on YouTube upon clicking corresponding box art
+    # Method to show movie trailer on YouTube upon clicking box art
     def open_trailer(self):
-        webbrowser.open(self.trailer)
+  wser.open(self.trailer)      webbro
